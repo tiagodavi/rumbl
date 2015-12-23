5c484af497955401160becc089afa7131dac9e1c
Tiago <tiago.asp.net@gmail.com>
2015-12-23 11:18:38 -0200

    Add authentication plug to some actions

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,11 +2,12 @@ defmodule Rumbl.UserController do
 	use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
   plug :scrub_params, "user" when action in [:create, :update]
 
 	def index(conn, _params) do
-		users = Repo.all(User)
-		render conn, "index.html", users: users
+    users = Repo.all(User)
+    render conn, "index.html", users: users
 	end
 
 	def show(conn, %{"id" => id}) do
@@ -24,10 +25,22 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 end
