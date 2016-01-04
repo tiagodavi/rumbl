4f9f642aeba14dca2d2e007cfdd48e76449ff1fc
Tiago <tiago.asp.net@gmail.com>
2016-01-04 16:18:12 -0200

    Add more tests to video

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,12 +1,58 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
+  alias Rumbl.Video
 
-  setup do
-    user = insert_user(username: "Max")
-    conn = assign(conn, :current_user, user)
-    {:ok, conn: conn, user: user}
+  @valid_attrs %{url: "http://youtu.be",title: "vid", description: "a vid"}
+  @invalid_attrs %{title: "invalid"}
+
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(conn, :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
   end
 
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+
+  @tag login_as: "max"
+  test "creates user video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+
+  @tag login_as: "max"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
+  end
+
+  @tag login_as: "max"
+  test "authorizes actions against access by other users", %{user: owner, conn: conn} do
+    video = insert_video(owner, @valid_attrs)
+    non_owner = insert_user(username: "sneaky")
+    conn = assign(conn, :current_user, non_owner)
+
+    assert_raise Ecto.NoResultsError, fn ->
+      get(conn, video_path(conn, :show, video))
+    end
+    assert_raise Ecto.NoResultsError, fn ->
+      get(conn, video_path(conn, :edit, video))
+    end
+    assert_raise Ecto.NoResultsError, fn ->
+      put(conn, video_path(conn, :update, video, video: @valid_attrs))
+    end
+    assert_raise Ecto.NoResultsError, fn ->
+      delete(conn, video_path(conn, :delete, video))
+    end
+  end
+
+  @tag login_as: "max"
   test "list all user's videos on index", %{conn: conn, user: user} do
     user_video  = insert_video(user, title: "Funny cats")
     other_video = insert_video(insert_user(username: "Other"), title: "Another video")
