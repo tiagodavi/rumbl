d60c967b83349cd73f67b78d92255d0f3aeab9ea
Tiago <tiago.asp.net@gmail.com>
2016-01-12 12:03:06 -0200

    Add protocol and custom type to slug of the video

---
+++ lib/rumbl/permalink.ex
@@ -0,0 +1,20 @@
+defmodule Rumbl.Permalink do
+  @behavior Ecto.Type
+
+  def type, do: :id
+
+  def cast(binary) when is_binary(binary) do
+    case Integer.parse(binary) do
+      {int, _} when int > 0 -> {:ok, int}
+      _ -> :error
+    end
+  end
+  def cast(integer) when is_integer(integer), do: {:ok, integer}
+  def cast(_), do: :error
+
+  def dump(integer) when is_integer(integer), do: {:ok, integer}
+  def dump(_), do: :error
+
+  def load(integer) when is_integer(integer), do: {:ok, integer}
+  def load(_), do: :error
+end
--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,13 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
+
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -12,7 +15,7 @@ defmodule Rumbl.Video do
   end
 
   @required_fields ~w(url title description)
-  @optional_fields ~w(category_id)
+  @optional_fields ~w(category_id slug)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -39,5 +42,11 @@ defmodule Rumbl.Video do
     str
     |> String.downcase()
     |> String.replace(~r/[^\w-]+/, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
