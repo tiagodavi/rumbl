cc0dc8c67f61f1fbe64b13b60dbc1fe132654d7b
Tiago <tiago.asp.net@gmail.com>
2016-01-13 15:32:51 -0200

    Add realtime annotations to videos

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule Rumbl.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", Rumbl.RoomChannel
+  channel "videos:*", Rumbl.VideoChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -18,11 +18,7 @@ import "deps/phoenix_html/web/static/js/phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import socket from "./socket"
+import Video from "./video"
 
-import Player from "./player"
-let video = document.getElementById("video")
-
-if(video){
-  Player.init(video.id, video.getAttribute("data-player-id"))
-}
+Video.init(socket, document.getElementById("video"))
